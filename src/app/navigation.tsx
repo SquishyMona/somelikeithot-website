f6ffcb1ab0cc6f790bc88292dbed74334bd9e53f
SquishyMona <kotonefemc@gmail.com>
2024-01-29 18:01:51 -0500

    got navigation done

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -5,26 +5,47 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Navigation() {
+    const [isOpen, setIsOpen] = useState(true);
+    const toggleMenu = () => {
+          setIsOpen(!isOpen);
+          console.log(isOpen);
+          if (isOpen) {
+            document.getElementById('dropdown').style.top = '0';
+            if (false) {
+              document.getElementById('navbar').style.height = '660px';
+            }
+            else {
+              document.getElementById('navbar').style.height = '600px';
+            }
+          }
+          else {
+            document.getElementById('dropdown').style.top = '-100vh';
+            document.getElementById('navbar').style.height = '60px';
+          }
+      }
+  
     return (
-        <>
-            <nav id="navbar" style={{maxHeight: '48px'}}>
-                <a className="icon"  >
-                    <i className="fa fa-bars"></i>
-                </a>
-                <img src="/Images/SLIHLogo.png"></img>
-                <a className="slihText" href="index.html" style={{display: 'inline-block', margin: '0'}}>Some Like It Hot</a>
-                <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="about-us.html">About Us</a></li>
-                    <li><a href="news.html">News</a></li>
-                    <li><a href="current-hotties.html">Current Hotties</a></li>
-                    <li><a href="repertoire.html">Repertoire</a></li>
-                    <li><a href="alumni.html">Alumni</a></li>
-                    <li><a href="media.html">Media</a></li>
-                    <li><a href="https://www.bonfire.com/store/some-like-it-hot/" target="_blank">Merch</a></li>
-                    <li><a href="contact-us.html">Contact</a></li>
+            <nav id="navbar">
+                <div className="hamburger-lines" onClick={toggleMenu}>
+                    <span className="line line1"></span>
+                    <span className="line line2"></span>
+                    <span className="line line3"></span>
+                </div>  
+                <div className="homelink">
+                    <img src="/Images/SLIHLogo.png"></img>
+                    <Link className="slihText" href="/" style={{display: 'inline-block', margin: '0'}}>Some Like It Hot</Link>
+                </div>
+                <ul id="dropdown">
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/about">About Us</Link></li>
+                    <li><Link href="/news">News</Link></li>
+                    <li><Link href="/currenthotties">Current Hotties</Link></li>
+                    <li><Link href="/repertoire">Repertoire</Link></li>
+                    <li><Link href="/alumni">Alumni</Link></li>
+                    <li><Link href="/media">Media</Link></li>
+                    <li><Link href="https://www.bonfire.com/store/some-like-it-hot/" target="_blank">Merch</Link></li>
+                    <li><Link href="/contact">Contact</Link></li>
                 </ul>
             </nav>
-        </>
     );
 }
